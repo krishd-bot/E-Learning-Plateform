6370eb5546811bd886dc43a5e579fd6c80030e35
Deepak Pandey <deepakpandey392003@gmail.com>
2026-08-08 00:35:37 +0530

    fix CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -32,11 +32,16 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
